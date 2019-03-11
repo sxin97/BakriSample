d141e855f3dabe6b4b0ab4da01e9b0d24257f140
Lee Sun Xin <sxin_97@hotmail.com>
2019-03-11 12:12:40 +0800

    Add project files.

---
+++ DAL/BaseRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class BaseRepo
+    {
+        private UniversityDBEntities context;
+       public UniversityDBEntities Context {
+            get {
+                if (context==null)
+                {
+                    context = new UniversityDBEntities();
+                }
+                return context;
+            }
+
+        }
+    }
+}
---
+++ DAL/Repo/CourseRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+    class CourseRepo:BaseRepo
+    {
+        public List<Course> GetAll()
+        {
+            return (from c in Context.Courses select c).ToList();
+        }
+        public List<Course> GetAll(int skip, int take)
+        {
+            return (from c in Context.Courses select c).Skip(skip).Take(take).ToList();
+        }
+        public Course GetById(string Id)
+        {
+            return (from c in Context.Courses where c.CourseCode==Id select c).FirstOrDefault();
+        }
+        public void Save(Course course)
+        {
+            Context.Courses.Add(course);
+            Context.SaveChanges();
+        }
+    }
+}
